0954bfae7d7adf55a4154d44ae432b3f1c708046
karirin <karirin419@gmail.com>
2023-07-22 01:07:16 +0900

    Initial Commit

---
+++ miss/missApp.swift
@@ -0,0 +1,17 @@
+//
+//  missApp.swift
+//  miss
+//
+//  Created by hashimo ryoya on 2023/07/22.
+//
+
+import SwiftUI
+
+@main
+struct missApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
